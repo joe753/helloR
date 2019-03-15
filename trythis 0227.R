d19ace6ca90d18f3b4841103dec4831884011507
joe753 <joe753@naver.com>
2019-03-15 18:11:45 +0900

    2019.03.15

--- trythis 0227.R
+++ trythis 0227.R
@@ -683,5 +683,129 @@ ggplot(data, aes(cls, kor)) +
 
 
 ggplot(try4, aes(poptotal, popasian)) +
-  geom_point(aes(color=poptotal, size=popasian), 
+  geom_point(aes(color=poptotal, size=popasian),
              alpha=0.3)
+
+
+
+
+
+library('ggplot2')
+library('dplyr')
+library(tibble)  
+install.packages('maps')
+install.packages('mapproj')
+install.packages('ggiraphExtra')
+
+library('ggiraphExtra')
+ggiraphExtra::ggChoropleth()
+
+
+chodata = data.frame(state = tolower(rownames(USArrests)), USArrests)
+
+usmap = map_data('state') 
+head(usmap)
+
+
+ggChoropleth(data=chodata,
+             aes(fill=Murder, map_id=state),
+             map = usmap,
+             title = '..',
+             reverse = F,
+             interactive = T)
+
+
+
+ ggplot(chodata, aes(map_id = state)) + 
+  geom_map(aes(fill = Murder), map = usmap) + 
+  expand_limits(x = usmap$long, y = usmap$lat) +
+  labs(title="USA Murder", fill = '살인')
+
+ tooltips = paste0(
+   sprintf("<p><strong>%s</strong></p>", as.character(chodata$state)),
+   '<table>',
+   '  <tr>',
+   '    <td>인구(만)</td>',
+   sprintf('<td>%.0f</td>', chodata$UrbanPop * 10),
+   '  </tr>',
+   '  <tr>',
+   '    <td>살인</td>',
+   sprintf('<td>%.0f</td>', chodata$Murder),
+   '  </tr>',
+   '  <tr>',
+   '    <td>폭력</td>',
+   sprintf('<td>%.0f</td>', chodata$Assault),
+   '  </tr>',
+   '</table>' )
+ 
+ tooltips
+
+ 
+ onclick = sprintf("alert(\"%s\")", as.character(chodata$state))
+onclick 
+install.packages("ggiraph")
+library(ggiraph)
+
+ggplot(chodata, aes(data = Murder, map_id = state)) +
+  geom_map_interactive( 
+    aes(fill = Murder,
+        data_id = state,
+        tooltip = stringi::stri_enc_toutf8(tooltips),
+        onclick = onclick), 
+    map = usmap) +
+  expand_limits(x = usmap$long, y = usmap$lat) +
+  scale_fill_gradient2('살인', low='red', high = "blue", mid = "green") +
+  labs(title="USA Murder") -> gg_map
+
+
+install.packages('stringi')
+######################
+
+setlib = function() {
+library(stringi)
+library('ggplot2')
+library('dplyr')
+library('ggiraphExtra')
+library(ggiraph)
+library(kormaps2014)
+}
+
+
+setlib
+
+gg_map
+
+options(encoding='UTF-8')
+
+ggiraph(code = print(gg_map))
+girafe(ggobj = gg_map)
+
+
+ggiraph(code = print(gg_map, options(encoding='utf-8')))
+
+
+
+ install.packages('devtools')
+
+ 
+ devtools::install_github("cardiomoon/kormaps2014")
+
+ library(kormaps2014)
+ options(encoding='utf-8')
+
+kdata = changeCode(korpop1)
+head(kdata)
+colnames(kdata)
+
+
+kdata = kdata %>% rename (pop = 총인구_명)
+kdata = kdata %>% rename (area = 행정구역별_읍면동)
+kdata$area = stringi::stri_enc_toutf8(kdata$area)
+
+head(kormap1)
+ 
+ggChoropleth(data=kdata, 
+              aes(fill = pop, map_id = code, tooltip = area),
+              map = kormap1,
+              interactive = T)
+ 
